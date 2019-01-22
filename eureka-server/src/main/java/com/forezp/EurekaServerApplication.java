7d50f147cd3fb1191bea47c47e48e4e1b5359872
terryecnu <terryecnu@sina.com>
2019-01-22 16:12:38 +0800

    change

--- eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -10,5 +10,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		
+		System.out.println("eureka");
 	}//
 }
